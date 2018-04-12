c459a07696da2fc87d3fdbb00e684606f97bca2d
jaean123 <whileloops@gmail.com>
2018-04-12 16:54:11 -0400

    Update PathTransmission.java

--- src/transmission/PathTransmission.java
+++ src/transmission/PathTransmission.java
@@ -16,13 +16,17 @@ public class PathTransmission implements Transmission {
         series = new XYChart.Series<>();
     }
 
-    @Override
-    public void processTransmission(ArrayList<Integer> signal) {
-        // Get initial point x and y
-        double x = signal.get(0);
-        double y = signal.get(1);
-        series.getData().add(new XYChart.Data<>(x, y));
+    public XYChart.Series<Double, Double> getSeries() {
+        return series;
+    }
 
+    @Override
+    public void processTransmission(ArrayList signal) {
+        // Get initial point x and y
+        double x = (Double) signal.get(0);
+        double y = (Double) signal.get(1);
+        series.getData().add(new XYChart.Data<>(x, y));
+//        System.out.println("x: " + x + ", y: " + y);
 
         /*for (int i = 3; i < signal.length()-1; i++) { TODO figure out and re-implement.
             // Subsequent data for are delta x and delta y values
